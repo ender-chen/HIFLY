0030b2befb92c96515c9682f322e15e6e2a5baf6
dong.chen <dong.chen@ck-telecom.com>
2015-08-28 10:02:12 +0800

    [feature]Far close follow feature. 	1.It can follow a moving target with far close motion. 		a.It only change the target when it reach the point. 	2.But it have some bugs 		a.Not a smooth moving. 		b.Some times it will crash down when change into offboard mode.
    Change-Id: Icc29641c936ce91aacc9aecb921a7b02684c2af0

---
+++ src/modules/navigator/fcf.cpp
@@ -0,0 +1,288 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2013-2014 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+/**
+ * @file navigator_fcf.cpp
+ * Helper class to access fcf
+ * @author dong chen <dong.chen@ck-telecom.com>
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <fcntl.h>
+
+#include <mavlink/mavlink_log.h>
+#include <systemlib/err.h>
+//#include <geo/geo.h>
+
+#include <uORB/uORB.h>
+#include <uORB/topics/mission.h>
+#include <uORB/topics/home_position.h>
+
+#include "navigator.h"
+#include "fcf.h"
+
+#define DELAY_SIGMA	0.01f
+
+FCF::FCF(Navigator *navigator, const char *name) :
+	NavigatorMode(navigator, name),
+	_fcf_state(FCF_STATE_NONE),
+	_fcf_item({0}),
+	_waypoint({0}),
+	_local_pos({0}),
+	_ref_pos({0}),
+	_ref_alt(0),
+	_fcf_item_reached(false),
+	_param_bottom_alt(this, "FCF_BOTTOM_ALT", false),
+	_param_top_alt(this, "FCF_TOP_ALT", false),
+	_param_hori_dist(this, "FCF_HORI_DIST", false),
+	_param_acceptance_radius(this, "FCF_ACC_RAD", false),
+	_param_rate_x(this, "FCF_RATE_X", false),
+	_param_rate_y(this, "FCF_RATE_Y", false),
+	_param_rate_z(this, "FCF_RATE_Z", false)
+{
+	/* load initial params */
+	updateParams();
+	/* initial reset */
+	on_inactive();
+}
+
+FCF::~FCF()
+{
+}
+
+void
+FCF::on_inactive()
+{
+	/* reset RTL state only if setpoint moved */
+	if (!_navigator->get_can_loiter_at_sp()) {
+		_fcf_state = FCF_STATE_NONE;
+	}
+}
+
+void
+FCF::on_activation()
+{
+	struct position_setpoint_triplet_s *pos_sp_triplet = _navigator->get_position_setpoint_triplet();
+	if (_fcf_state == FCF_STATE_NONE) {
+			_fcf_state = FCF_STATE_CLIMB;
+	}
+	waypoint_update();
+	update_ref();
+	target_local_position_update();
+	set_fcf_item();
+	fcf_item_to_position_setpoint(&_fcf_item, &pos_sp_triplet->current);
+	_navigator->set_position_setpoint_triplet_updated();
+}
+
+void
+FCF::on_active()
+{
+	struct position_setpoint_triplet_s *pos_sp_triplet = _navigator->get_position_setpoint_triplet();
+	if (is_fcf_item_reached()) {
+		waypoint_update();
+		update_ref();
+		target_local_position_update();
+		advance_fcf();
+		set_fcf_item();
+		fcf_item_to_position_setpoint(&_fcf_item, &pos_sp_triplet->current);
+		_navigator->set_position_setpoint_triplet_updated();
+	}
+}
+
+
+
+
+void
+FCF::fcf_item_to_position_setpoint(const struct fcf_item_s *item, struct position_setpoint_s *sp)
+{
+	follow_strategy();
+	sp->x = item->x;
+	sp->y = item->y;
+	sp->z = item->z;
+	sp->valid = true;
+	sp->position_valid = true;
+}
+
+void
+FCF::follow_strategy()
+{
+}
+
+void
+FCF::waypoint_update()
+{
+	bool waypoint_updated = false;
+	orb_check(_navigator->get_waypoint_sub(), &waypoint_updated);
+	if (waypoint_updated) {
+		orb_copy(ORB_ID(waypoint), _navigator->get_waypoint_sub(), &_waypoint);
+	}
+}
+
+void
+FCF::target_local_position_update()
+{
+	double lat_sp = _waypoint.lat;
+	double lon_sp = _waypoint.lon;
+	float alt_sp = _waypoint.alt;
+	map_projection_project(&_ref_pos, lat_sp, lon_sp, &_local_pos.x, &_local_pos.y);
+	_local_pos.z = -(alt_sp - _ref_alt);
+}
+
+void
+FCF::update_ref()
+{
+	map_projection_init(&_ref_pos, _navigator->get_local_position()->ref_lat, _navigator->get_local_position()->ref_lon); 
+	_ref_alt = _navigator->get_local_position()->ref_alt;
+}
+
+void
+FCF::set_fcf_item()
+{
+	/* make sure we have the latest params */
+	updateParams();
+
+	_navigator->set_can_loiter_at_sp(false);
+
+	switch (_fcf_state) {
+	case FCF_STATE_CLIMB: {
+		float climb_alt = _param_bottom_alt.get();
+		_fcf_item.x = get_target_local_position() -> x;
+		_fcf_item.y = get_target_local_position() -> y;
+		_fcf_item.z = -climb_alt;
+		_fcf_item.yaw = NAN;
+
+		mavlink_log_critical(_navigator->get_mavlink_fd(), "FCF: Move to the target position");
+		break;
+	}
+
+	case FCF_STATE_RIGHT_GO: {
+		float top_alt = _param_top_alt.get();
+		_fcf_item.x = get_target_local_position() -> x + _param_hori_dist.get();
+		_fcf_item.y = get_target_local_position() -> y;
+		_fcf_item.z = -top_alt;
+		_fcf_item.yaw = NAN;
+
+		mavlink_log_critical(_navigator->get_mavlink_fd(), "GO TO RIGHT");
+		break;
+	}
+
+	case FCF_STATE_RIGHT_BACK: {
+		float bottom_alt = _param_bottom_alt.get();
+		_fcf_item.x = get_target_local_position() -> x;
+		_fcf_item.y = get_target_local_position() -> y;
+		_fcf_item.z = -bottom_alt;
+		_fcf_item.yaw = NAN;
+
+		mavlink_log_critical(_navigator->get_mavlink_fd(), "FCF: right back to base alt.");
+		break;
+	}
+
+	case FCF_STATE_LEFT_GO: {
+		float top_alt = _param_top_alt.get();
+		_fcf_item.x = get_target_local_position() -> x - _param_hori_dist.get();
+		_fcf_item.y = get_target_local_position() -> y;
+		_fcf_item.z = -top_alt;
+		_fcf_item.yaw = NAN;
+
+		mavlink_log_critical(_navigator->get_mavlink_fd(), "FCF: left go to top alt.");
+		break;
+	}
+	case FCF_STATE_LEFT_BACK: {
+		float bottom_alt = _param_bottom_alt.get();
+		_fcf_item.x = get_target_local_position() -> x;
+		_fcf_item.y = get_target_local_position() -> y;
+		_fcf_item.z = -bottom_alt;
+		_fcf_item.yaw = NAN;
+
+		mavlink_log_critical(_navigator->get_mavlink_fd(), "FCF: left back to base alt.");
+		break;
+	}
+
+	default:
+		break;
+	}
+
+	reset_fcf_item_reached();
+}
+
+void
+FCF::advance_fcf()
+{
+	switch (_fcf_state) {
+	case FCF_STATE_CLIMB:
+		_fcf_state = FCF_STATE_RIGHT_GO;
+		break;
+
+	case FCF_STATE_RIGHT_GO:
+		_fcf_state = FCF_STATE_RIGHT_BACK;
+		break;
+
+	case FCF_STATE_RIGHT_BACK:
+		_fcf_state = FCF_STATE_LEFT_GO;
+		break;
+
+	case FCF_STATE_LEFT_GO:
+		_fcf_state = FCF_STATE_LEFT_BACK;
+		break;
+
+	case FCF_STATE_LEFT_BACK:
+		_fcf_state = FCF_STATE_RIGHT_GO;
+		break;
+
+	default:
+		break;
+	}
+}
+
+bool
+FCF::is_fcf_item_reached()
+{
+	if(!_fcf_item_reached)
+	{
+		float dist = -1.0f;
+		float dist_xy = -1.0f;
+		float dist_z = -1.0f; 
+		dist = mavlink_wpm_distance_to_point_local(
+							  _navigator->get_local_position()->x,
+							  _navigator->get_local_position()->y,
+							  _navigator->get_local_position()->z,
+							  _fcf_item.x, _fcf_item.y, _fcf_item.z, &dist_xy, &dist_z);
+		if(dist <= _param_acceptance_radius.get())
+		{
+			_fcf_item_reached = true;
+			return true;
+		}
+	}
+	return false;
+}
--- src/modules/navigator/navigator.h
+++ src/modules/navigator/navigator.h
@@ -72,11 +72,12 @@
 #include "land.h"
 #include "idle.h"
 #include "follow.h"
+#include "fcf.h"
 
 /**
  * Number of navigation modes that need on_active/on_inactive calls
  */
-#define NAVIGATOR_MODE_ARRAY_SIZE 11
+#define NAVIGATOR_MODE_ARRAY_SIZE 12
 
 class Navigator : public control::SuperBlock
 {
@@ -137,6 +138,7 @@ public:
 	struct vehicle_status_s*	    get_vstatus() { return &_vstatus; }
 	struct vehicle_control_mode_s*	    get_control_mode() { return &_control_mode; }
 	struct vehicle_global_position_s*   get_global_position() { return &_global_pos; }
+	struct vehicle_local_position_s*	get_local_position() { return &_local_pos; }
 	struct vehicle_gps_position_s*	    get_gps_position() { return &_gps_pos; }
 	struct sensor_combined_s*	    get_sensor_combined() { return &_sensor_combined; }
 	struct home_position_s*		    get_home_position() { return &_home_pos; }
@@ -182,6 +184,7 @@ private:
 	int		_mavlink_fd;			/**< the file descriptor to send messages over mavlink */
 
 	int		_global_pos_sub;		/**< global position subscription */
+	int 	_local_pos_sub;			/**< local position subscription */
 	int		_gps_pos_sub;		/**< gps position subscription */
 	int		_sensor_combined_sub;		/**< sensor combined subscription */
 	int		_home_pos_sub;			/**< home position subscription */
@@ -204,6 +207,7 @@ private:
 	vehicle_status_s				_vstatus;		/**< vehicle status */
 	vehicle_control_mode_s				_control_mode;		/**< vehicle control mode */
 	vehicle_global_position_s			_global_pos;		/**< global vehicle position */
+	vehicle_local_position_s 			_local_pos;			/**< local vehicle position */
 	vehicle_gps_position_s				_gps_pos;		/**< gps position */
 	sensor_combined_s				_sensor_combined;	/**< sensor values */
 	home_position_s					_home_pos;		/**< home position for RTL */
@@ -236,7 +240,8 @@ private:
 							  OBC rules (rc loss mode) */
 	TAKEOFF	_takeoff;
 	LAND  		_land;
-	IDLE                   _idle;
+	IDLE        _idle;
+	FCF 		_fcf;				/**< class that handles the far close follow mission */
 	Follow		_follow;
 	DataLinkLoss	_dataLinkLoss;			/**< class that handles the OBC datalink loss mode */
 	EngineFailure	_engineFailure;			/**< class that handles the engine failure mode
@@ -258,6 +263,11 @@ private:
 	 * Retrieve global position
 	 */
 	void		global_position_update();
+
+	/**
+	 * Retrieve local position
+	 */
+	void		local_position_update();
 
 	/**
 	 * Retrieve gps position
