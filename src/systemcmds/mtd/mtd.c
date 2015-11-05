97fac09a66ede6a571946b5f1568679641aa8ee7
mingliang.wu <mingliang.wu@ck-telecom.com>
2015-11-05 10:49:41 +0800

    [BugFix] solved that parameters losts sometimes after rebooting the mav
    [id]:102554
    
    [Type]
        BugFix
    
    [Products]
       HIFLY
    
    [Requirement]
        N/A
    
    [Solution Description]
        support vfat filesystem for mtd
    
    [Other Info]
        N/A
    
    Change-Id: Ibecfe2a7a9415fd4963770f4ef108d662456130b

--- src/systemcmds/mtd/mtd.c
+++ src/systemcmds/mtd/mtd.c
@@ -92,7 +92,7 @@ static void	mtd_erase(char *partition_names[], unsigned n_partitions);
 static void	mtd_readtest(char *partition_names[], unsigned n_partitions);
 static void	mtd_rwtest(char *partition_names[], unsigned n_partitions);
 static void	mtd_print_info(void);
-static int	mtd_get_geometry(int index, unsigned long *blocksize, unsigned long *erasesize, unsigned long *neraseblocks, 
+static int	mtd_get_geometry(unsigned long *blocksize, unsigned long *erasesize, unsigned long *neraseblocks, 
 	unsigned *blkpererase, unsigned *nblocks, unsigned *partsize, unsigned n_partitions);
 
 static bool attached = false;
@@ -101,9 +101,7 @@ static struct mtd_dev_s *mtd_dev;
 static unsigned n_partitions_current = 0;
 
 /* note, these will be equally sized */
-static char *partition_names_default[] = {"/fs/mtd_params", "/fs/mtd_waypoints", "/fs/mtd_log"};
-static int partition_block_offset[] = {0, 8, 16};
-static int partition_block_num[] = {8, 8, 496};
+static char *partition_names_default[] = {"/dev/mtd"};
 static const int n_partitions_default = sizeof(partition_names_default) / sizeof(partition_names_default[0]);
 
 static void
@@ -269,12 +267,12 @@ mtd_start(char *partition_names[], unsigned n_partitions)
 		exit(1);
 	}
 
-	//unsigned long blocksize, erasesize, neraseblocks;
-	//unsigned blkpererase, nblocks, partsize;
+	unsigned long blocksize, erasesize, neraseblocks;
+	unsigned blkpererase, nblocks, partsize;
 
-	//ret = mtd_get_geometry(&blocksize, &erasesize, &neraseblocks, &blkpererase, &nblocks, &partsize, n_partitions);
-	//if (ret)
-	//	exit(3);
+	ret = mtd_get_geometry(&blocksize, &erasesize, &neraseblocks, &blkpererase, &nblocks, &partsize, n_partitions);
+	if (ret)
+		exit(3);
 
 	/* Now create MTD FLASH partitions */
 
@@ -284,16 +282,15 @@ mtd_start(char *partition_names[], unsigned n_partitions)
 	unsigned offset;
 	unsigned i;
 
-	for (i = 0; i < n_partitions; i++) {
+	for (offset = 0, i = 0; i < n_partitions; offset += nblocks, i++) {
 
 		/* Create the partition */
-		offset = partition_block_offset[i];
 
-		part[i] = mtd_partition(mtd_dev, offset, partition_block_num[i]);
+		part[i] = mtd_partition(mtd_dev, offset, nblocks);
 
 		if (!part[i]) {
 			warnx("ERROR: mtd_partition failed. offset=%lu nblocks=%lu",
-			      (unsigned long)offset, (unsigned long)partition_block_num[i]);
+			      (unsigned long)offset, (unsigned long)nblocks);
 			exit(4);
 		}
 
@@ -324,7 +321,7 @@ mtd_start(char *partition_names[], unsigned n_partitions)
 	exit(0);
 }
 
-int mtd_get_geometry(int index, unsigned long *blocksize, unsigned long *erasesize, unsigned long *neraseblocks, 
+int mtd_get_geometry(unsigned long *blocksize, unsigned long *erasesize, unsigned long *neraseblocks, 
 	unsigned *blkpererase, unsigned *nblocks, unsigned *partsize, unsigned n_partitions)
 {
 		/* Get the geometry of the FLASH device */
@@ -348,8 +345,7 @@ int mtd_get_geometry(int index, unsigned long *blocksize, unsigned long *erasesi
 	 */
 
 	*blkpererase = geo.erasesize / geo.blocksize;
-	//*nblocks     = (geo.neraseblocks / n_partitions) * *blkpererase;
-	*nblocks     = partition_block_num[index];
+	*nblocks     = (geo.neraseblocks / n_partitions) * *blkpererase;
 	*partsize    = *nblocks * geo.blocksize;
 
 	return ret;
@@ -358,12 +354,12 @@ int mtd_get_geometry(int index, unsigned long *blocksize, unsigned long *erasesi
 /*
   get partition size in bytes
  */
-static ssize_t mtd_get_partition_size(int index)
+static ssize_t mtd_get_partition_size(void)
 {
 	unsigned long blocksize, erasesize, neraseblocks;
 	unsigned blkpererase, nblocks, partsize = 0;
 
-	int ret = mtd_get_geometry(index, &blocksize, &erasesize, &neraseblocks, &blkpererase, &nblocks, &partsize, n_partitions_current);
+	int ret = mtd_get_geometry(&blocksize, &erasesize, &neraseblocks, &blkpererase, &nblocks, &partsize, n_partitions_current);
 	if (ret != OK) {
 		errx(1, "Failed to get geometry");
 	}
@@ -375,14 +371,10 @@ void mtd_print_info(void)
 	if (!attached)
 		exit(1);
 
-	unsigned long blocksize = 0;
-	unsigned long erasesize = 0;
-    unsigned long neraseblocks = 0;
-	unsigned blkpererase = 0;
-	unsigned nblocks = 0;
-	unsigned partsize = 0;
+	unsigned long blocksize, erasesize, neraseblocks;
+	unsigned blkpererase, nblocks, partsize;
 
-	int ret = mtd_get_geometry(0, &blocksize, &erasesize, &neraseblocks, &blkpererase, &nblocks, &partsize, n_partitions_current);
+	int ret = mtd_get_geometry(&blocksize, &erasesize, &neraseblocks, &blkpererase, &nblocks, &partsize, n_partitions_current);
 	if (ret)
 		exit(3);
 
@@ -433,10 +425,10 @@ mtd_erase(char *partition_names[], unsigned n_partitions)
 void
 mtd_readtest(char *partition_names[], unsigned n_partitions)
 {
+	ssize_t expected_size = mtd_get_partition_size();
 
 	uint8_t v[128];
 	for (uint8_t i = 0; i < n_partitions; i++) {
-		ssize_t expected_size = mtd_get_partition_size(i);
 		ssize_t count = 0;
 		printf("reading %s expecting %u bytes\n", partition_names[i], expected_size);
 		int fd = open(partition_names[i], O_RDONLY);
@@ -464,10 +456,10 @@ mtd_readtest(char *partition_names[], unsigned n_partitions)
 void
 mtd_rwtest(char *partition_names[], unsigned n_partitions)
 {
+	ssize_t expected_size = mtd_get_partition_size();
 
 	uint8_t v[128], v2[128];
 	for (uint8_t i = 0; i < n_partitions; i++) {
-		ssize_t expected_size = mtd_get_partition_size(i);
 		ssize_t count = 0;
         off_t offset = 0;
 		printf("rwtest %s testing %u bytes\n", partition_names[i], expected_size);
