c81b761cb1e6e1ca19509ed4f1768142f78c2fd3
tumbili <bapstr@ethz.ch>
2016-01-28 16:08:11 +0100

    vtol: reset multirotor positon and altitude when not in rotary wing mode
    (cherry picked from commit f8f208dbe6f1747e014bbdeca955662b5bfb62cb)
    
    Change-Id: I0b1f5fe5f7957bcd32f3e5554fa9635d009761c3

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -1622,9 +1622,13 @@ void MulticopterPositionControl::control_follow_loiter(float dt)
 
 void MulticopterPositionControl::control_auto(float dt)
 {
-	if (!_mode_auto) {
+	if (!_mode_auto || _vehicle_status.in_transition_mode || !_vehicle_status.is_rotary_wing) {
 		_mode_auto = true;
 		/* reset position setpoint on AUTO mode activation */
+		if (_vehicle_status.in_transition_mode || !_vehicle_status.is_rotary_wing) {
+			_reset_pos_sp = true;
+			_reset_alt_sp = true;
+		}
 		reset_pos_sp();
 		reset_alt_sp();
 		/* set current velocity as last target velocity to smooth out transition */
