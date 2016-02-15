d7b897419d1a36d18de74f2d752b663a34bbcc12
liang.tang <liang.tang@ck-telecom.com>
2016-02-15 17:10:50 +0800

    [feature] follow: add follow common
    Change-Id: Iea1c1bbdd789b797229e7d73520aa53d07b040fe

---
+++ src/modules/navigator/follow_common.cpp
@@ -0,0 +1,113 @@
+/*copyright (c) 2013-2014 PX4 Development Team. All rights reserved.
+ *
+ *Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+/**
+ * @file follow.cpp
+ *
+ * Helper class to access follow
+ *
+ * @author liang.tang<liang.tang@ck-telecom.com>
+ */
+
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <float.h>
+
+#include <mavlink/mavlink_log.h>
+#include <systemlib/err.h>
+#include <geo/geo.h>
+
+#include "navigator.h"
+#include "follow_common.h"
+
+#define WAYPOINT_TIMEOUT 3000000
+
+FollowCommon::FollowCommon(Navigator *navigator, const char *name) :
+	MissionBlock(navigator, name),
+	_follow_state(FOLLOW_STATE_NONE)
+{
+	updateParams();
+	on_inactive();
+}
+
+FollowCommon::~FollowCommon() {
+
+}
+
+void
+FollowCommon::on_inactive() {
+
+}
+
+void
+FollowCommon::on_activation() {
+	_follow_state = FOLLOW_STATE_NONE;
+}
+
+void
+FollowCommon::on_active() {
+	if (!_navigator->get_vstatus()->condition_landed) {
+
+		struct waypoint_s* waypoint_sp = _navigator->get_waypoint_sp();
+		if (is_valid_follow_item(waypoint_sp)) {
+			advance_follow();
+			set_follow_item(waypoint_sp);
+		}
+	}
+}
+
+void
+FollowCommon::set_follow_item(const struct waypoint_s *waypoint) {
+
+}
+
+void
+FollowCommon::advance_follow()
+{
+	switch (_follow_state) {
+		case FOLLOW_STATE_NONE:
+			_follow_state = FOLLOW_STATE_INIT;
+		break;
+
+		case FOLLOW_STATE_INIT:
+			_follow_state = FOLLOW_STATE_FOLLOW;
+		break;
+
+		default:
+		break;
+	}
+}
+
+bool
+FollowCommon::is_valid_follow_item(const struct waypoint_s *waypoint) {
+	return !(waypoint->timestamp == 0 || hrt_elapsed_time(&waypoint->timestamp) > WAYPOINT_TIMEOUT);
+}
---
+++ src/modules/navigator/follow_common.h
@@ -0,0 +1,87 @@
+/*copyright (c) 2013-2014 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+/**
+ * @file follow.h
+ *
+ * Navigator mode to access missions
+ *
+ * @author liang.tang <liang.tang@ck-telecom.com>
+ */
+
+#ifndef NAVIGATOR_FOLLOW_COMMON_H
+#define NAVIGATOR_FOLLOW_COMMON_H
+
+#include <drivers/drv_hrt.h>
+
+#include <controllib/blocks.hpp>
+#include <controllib/block/BlockParam.hpp>
+
+#include <uORB/topics/waypoint.h>
+#include "navigator_mode.h"
+#include "mission_block.h"
+
+#define MIN_FOLLOW_DIST 0.0f
+#define MAX_FOLLOW_DIST 30.0f
+#define MIN_FOLLOW_ALT 0.0f
+#define MAX_FOLLOW_ALT 30.0f
+
+class Navigator;
+
+class FollowCommon : public MissionBlock
+{
+public:
+	FollowCommon(Navigator *navigator, const char *name);
+
+	virtual ~FollowCommon();
+
+	virtual void on_inactive();
+
+	virtual void on_activation();
+
+	virtual void on_active();
+
+	virtual void set_follow_item(const struct waypoint_s *waypoint);
+
+	virtual bool is_valid_follow_item(const struct waypoint_s *waypoint);
+
+	virtual void advance_follow();
+
+	float _vehicle_ref_alt;
+	float _waypoint_ref_alt;
+
+	enum FollowState {
+		FOLLOW_STATE_NONE = 0,
+		FOLLOW_STATE_INIT,
+		FOLLOW_STATE_FOLLOW,
+	} _follow_state;
+};
+
+#endif
