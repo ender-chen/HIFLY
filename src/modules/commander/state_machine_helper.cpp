e8af6e8f2d176c6852285b7a3edcff1289b52ba4
dong.chen <dong.chen@ck-telecom.com>
2016-05-23 15:31:20 +0800

    [Bugfix]Lost signal, Repeated RTL.
    	1. When landed, Get outof RTL, Enter Posctl.
    	2. Takeoff Detected.
    	3. Get into RTL.
    
    Change-Id: Ib4de4c8542d627f725e5c26bcb71f7f058a2133b

--- src/modules/commander/state_machine_helper.cpp
+++ src/modules/commander/state_machine_helper.cpp
@@ -642,7 +642,7 @@ bool set_nav_state(struct vehicle_status_s *status, const bool data_link_loss_en
 	case vehicle_status_s::MAIN_STATE_FOLLOW_FC_ARC:
 	case vehicle_status_s::MAIN_STATE_FOLLOW_LOITER:
 		/* require RC for all manual modes */
-		if ((status->rc_signal_lost || status->rc_signal_lost_cmd) && armed && !status->condition_landed) {
+		if ((status->rc_signal_lost || status->rc_signal_lost_cmd) && armed) {
 			status->failsafe = true;
 
 			if (status->condition_global_position_valid && status->condition_home_position_valid) {
